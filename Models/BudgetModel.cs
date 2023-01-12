61069b59e01de86be268066ffefe1baeaed7c34a
jyotsna-a <109755472+jyotsna-a@users.noreply.github.com>
2023-01-12 12:40:34 -0600

    Simplified code.

--- Models/BudgetModel.cs
+++ Models/BudgetModel.cs
@@ -16,7 +16,42 @@ namespace ShopAid.Models
         public static List<BudgetModel> GetBudgets()
         {
             string dir = CurrentPath.GetDbasePath() + "\\Budget.txt";
-            return BudgetInputOutput.GetBudgetData(dir);
+            List<BudgetModel> budgets = new List<BudgetModel>();
+            string line = string.Empty;
+
+            try
+            {
+               //Check if the file exists
+               if (File.Exists(dir))
+               {
+                    //Create a Stream Reader
+                    using (StreamReader rdr = new StreamReader(dir))
+                    {
+                        //Read the data in the file
+                        while ((line = rdr.ReadLine()) != null)
+                        {
+                            var value = line.Split('|');
+
+                            //Add data to the Customers Model
+                            budgets.Add(new BudgetModel()
+                            {
+                                ID = int.Parse(value[0]),
+                                Budget = double.Parse(value[1])
+                            });
+                        }
+                    }
+               }
+               else
+               {
+                    throw new Exception("File Not Found!");
+               }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+             return budgets;
         }
 
         public static void EditBudgets(int id, double oldB, double newB)
