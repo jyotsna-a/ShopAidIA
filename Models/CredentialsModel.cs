63143e6fa09a0c87d08573d43a42b0e9dd87ddb1
jyotsna-a <109755472+jyotsna-a@users.noreply.github.com>
2023-01-12 12:43:15 -0600

    Simplified Credentials code

--- Models/CredentialsModel.cs
+++ Models/CredentialsModel.cs
@@ -1,6 +1,7 @@
 ﻿using ShopAid.Processes;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -28,7 +29,43 @@ namespace ShopAid.Models
         public static List<CredentialsModel> GetCredentials()
         {
             string dir = CurrentPath.GetDbasePath() + "\\Credentials.txt";
-            return CredentialInputOutput.GetCredentialData(dir);
+            List<CredentialsModel> credentials = new List<CredentialsModel>();
+            string line = string.Empty;
+
+            try
+            {
+                //Check if the file exists
+                if (File.Exists(dir))
+                {
+                    //Create a Stream Reader
+                    using (StreamReader rdr = new StreamReader(dir))
+                    {
+                        //Read the data in the file
+                        while ((line = rdr.ReadLine()) != null)
+                        {
+                            var value = line.Split('|');
+
+                            //Add data to the Customers Model
+                            credentials.Add(new CredentialsModel()
+                            {
+                                ID = int.Parse(value[0]),
+                                UserName = value[1],
+                                Password = value[2]
+                            });
+                        }
+                    }
+                }
+                else
+                {
+                    throw new Exception("File Not Found!");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+
+            return credentials;
         }
 
     }
